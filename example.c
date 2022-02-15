a3d19e31d54fbb455a286c898d7e3e9adea541bd
ekinakkaya <ekoeko0654@gmail.com>
2022-02-15 14:25:01 +0300

    random-noise generation with rand()

--- example.c
+++ example.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "easyppm.h"
 
 int main() {
+    srand(time(NULL));
+
     ppmdata_t *ppmdata = ppm_init(200, 200);
 
-    ppm_paint_fill(ppmdata, 100, 100, 120, 120, 200, 100, 50);
-    ppm_save_to_file(ppmdata, "test.ppm");
+    for (int i = 0; i < ppmdata->x; i++) {
+        for (int j = 0; j < ppmdata->y; j++) {
+            ppm_write_to_pixel(ppmdata, i, j, rand() % 255, rand() % 255, rand() % 255);
+        }
+    }
+
+    printf("ppmdata x: %d | y: %d\n", ppmdata->x, ppmdata->y);
+    ppm_print_chunk(ppmdata, 0, 0, 10, 10);
+    
+    ppm_save_to_file(ppmdata, "random-noise.ppm");
+
+    ppm_free(ppmdata);
 
     // it works!!
 
